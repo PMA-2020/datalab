652e2464c215a9c86af55c8aff54e6e8efb1d1bc
Jason Truluck <jason@smartlogic.io>
2017-09-07 15:19:22 -0400

    Add basic chart initialization

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,5 +1,11 @@
 import 'bootstrap';
+import 'bootstrap-select';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-select/dist/css/bootstrap-select.css';
 import 'font-awesome/css/font-awesome.css';
-import chart_helper from './chart_helper';
-// const f = chart_helper.filterData;
+// import chart_helper from './chart_helper';
+
+import chart from './chart';
+
+chart.initialize();
+
