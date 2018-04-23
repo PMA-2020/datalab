f7d28bd3894e75a74fcf61773c0bcfccf84d5094
bciar <bciar@yandex.com>
2018-04-23 08:28:52 -0700

    change overtime tooltip into dynamic set by javscript

--- source/javascripts/tooltip.js
+++ source/javascripts/tooltip.js
@@ -12,9 +12,15 @@ const enablePieChart = () => {
 		title: 'Pie charts are available to graph indicators that present data on various parts or portions that make up a whole. For example, method mix, which shows the portion of contraceptive users by each method type, may be graphed with a pie chart.'
 	});
 }
+const enableOverTime = () => {
+	$('.overtime-wrapper').tooltip({
+		title: 'To chart data overtime, select multiple rounds of data from the same country.'
+	});
+}
 const initialize = () => {
 	enablePieChart();
 	enableBtnSubmitChart();
+	enableOverTime();
 };
 const tooltips = {
 	initialize,
